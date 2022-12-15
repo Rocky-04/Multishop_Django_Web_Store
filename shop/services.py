6377b848ac161d7f601a5d9c786a6a5fc1e7cc3d
Rocky-04 <olegtsynkovskiy@gmail.com>
2022-12-15 11:51:34 +0200

    shop: add docstring for services and shop_tags

--- shop/services.py
+++ shop/services.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 
 def get_filter_products(limit: int = 99999, **kwargs) -> QuerySet:
     """
-        Retrieves a queryset of products that match the specified filters.
+    Retrieves a queryset of products that match the specified filters.
 
     :param limit: The maximum number of products to return (defaults to 99999)
     :param kwargs: Filters to apply to the products queryset (e.g. category="Clothing")
@@ -41,7 +41,7 @@ def get_filter_products(limit: int = 99999, **kwargs) -> QuerySet:
 
 def get_review_for_user_and_product(user_id: int, product_id: int) -> Optional[Reviews]:
     """
-        Retrieves the review (if any) that the specified user has left for the specified product.
+    Retrieves the review (if any) that the specified user has left for the specified product.
 
     :param user_id: The ID of the user to retrieve the review for.
     :param product_id: The ID of the product to retrieve the review for.
@@ -56,7 +56,7 @@ def get_review_for_user_and_product(user_id: int, product_id: int) -> Optional[R
 
 def get_rating_html(rating: int = 5) -> str:
     """
-        Draws product rating stars based on average rating.
+    Draws product rating stars based on average rating.
 
     :param rating: The average rating of the product, on a scale of 1 to 5.
     :return: A string containing the HTML for the star rating.
@@ -80,8 +80,8 @@ def get_rating_html(rating: int = 5) -> str:
 
 def get_tag_by_banner(pk_banner: int) -> Tag:
     """
-        Gets the tag for a given banner.
-    If no banner with the given primary key exists, return the last tag in the database as a default
+    Gets the tag for a given banner. If no banner with the given primary key exists, return the
+    last tag in the database as a default.
 
     :param pk_banner: The primary key of the banner.
     :return:  The tag for the given banner.
@@ -100,7 +100,7 @@ def get_tag_by_banner(pk_banner: int) -> Tag:
 
 def filter_colors_by_products(product_list_pk: list) -> MultilingualQuerySet:
     """
-        Gets the color filter for a list of products.
+    Gets the color filter for a list of products.
 
     :param product_list_pk: The primary keys of the products to filter.
     :return: A queryset of colors that are associated with the given products,
@@ -114,7 +114,7 @@ def filter_colors_by_products(product_list_pk: list) -> MultilingualQuerySet:
 
 def filter_size_by_products(product_list_pk: list) -> QuerySet:
     """
-        Gets the size filter for a list of products.
+    Gets the size filter for a list of products.
 
     :param product_list_pk: The primary keys of the products to filter.
     :return: A queryset of size that are associated with the given products,
@@ -129,7 +129,7 @@ def filter_size_by_products(product_list_pk: list) -> QuerySet:
 
 def filter_manufacturers_by_products(product_list_pk: list) -> QuerySet:
     """
-        Gets the manufacturer filter for a list of products.
+    Gets the manufacturer filter for a list of products.
 
     :param product_list_pk: The primary keys of the products to filter.
     :return: A queryset of manufacturers that are associated with the given products,
@@ -144,7 +144,7 @@ def filter_manufacturers_by_products(product_list_pk: list) -> QuerySet:
 
 def convert_str_to_int_list(string_of_numbers: str) -> list:
     """
-        Converts a string of numbers in list format to a list of integers.
+    Converts a string of numbers in list format to a list of integers.
 
     :param string_of_numbers: The string to convert, in the format '[25, 14, 13, 20]'.
     :return: A list of integers that were extracted from the input string.
@@ -156,7 +156,7 @@ def convert_str_to_int_list(string_of_numbers: str) -> list:
 
 def get_product_ids(data: Union[str, QuerySet, None] = '') -> list:
     """
-        Forms a list of product identifiers based on the given data.
+    Forms a list of product identifiers based on the given data.
 
     :param data: The data to use to generate the list of product identifiers.
             This can be a string of numbers in list format, a queryset of products,
@@ -183,7 +183,7 @@ def get_product_ids(data: Union[str, QuerySet, None] = '') -> list:
 
 def get_filtered_products_by_color(color: list) -> QuerySet:
     """
-        Gets the products that match the given color filter.
+    Gets the products that match the given color filter.
     If no color filter is specified, get all products.
 
     :param color: A list of colors to filter by.
@@ -199,7 +199,7 @@ def get_filtered_products_by_color(color: list) -> QuerySet:
 
 def get_filtered_products_by_size(size: list) -> QuerySet:
     """
-        Gets the products that match the given size filter.
+    Gets the products that match the given size filter.
     If no size filter is specified, get all products.
 
     :param size: A list of size to filter by.
@@ -215,7 +215,7 @@ def get_filtered_products_by_size(size: list) -> QuerySet:
 
 def get_filtered_products_by_manufacturer(brand: list) -> QuerySet:
     """
-        Gets the products that match the given brand filter.
+    Gets the products that match the given brand filter.
     If no brand filter is specified, get all products.
 
     :param brand: A list of brand to filter by.
@@ -231,7 +231,7 @@ def get_filtered_products_by_manufacturer(brand: list) -> QuerySet:
 
 def apply_product_filters(request: WSGIRequest, pk_list: list) -> QuerySet:
     """
-        Filters a list of products based on criteria specified in a WSGIRequest object.
+    Filters a list of products based on criteria specified in a WSGIRequest object.
     The function filters the product list using criteria such as minimum and maximum price,
     color, size, and manufacturer.
 
@@ -266,7 +266,7 @@ def apply_product_filters(request: WSGIRequest, pk_list: list) -> QuerySet:
 
 def get_nested_category_ids(category_slug: str) -> List[int]:
     """
-        Gets a list of primary keys for the given category and its nested subcategories.
+    Gets a list of primary keys for the given category and its nested subcategories.
 
     :param category_slug: The slug of the category to get the nested subcategories for.
     :return: A list of primary keys for the given category and its nested subcategories.
@@ -278,14 +278,12 @@ def get_nested_category_ids(category_slug: str) -> List[int]:
 
 def send_contact_form_message(request: WSGIRequest) -> None:
     """
-        Sends a message from a user using a contact form on a website.
+    Sends a message from a user using a contact form on a website.
 
     :param request: A WSGIRequest object containing the message to send.
     :return: None.
     """
-    """
-    Sends a message from a user using a contact form on a website.
-    """
+
     data = request.POST
     name = data.get('name')
     email = data.get('email')
@@ -319,7 +317,7 @@ def send_contact_form_message(request: WSGIRequest) -> None:
 
 def get_product_active_color(product: Product, color: Union[str, None]) -> AttributeColor:
     """
-        Gets the active color for a given product.
+    Gets the active color for a given product.
 
     :param product: The product to get the active color for.
     :param color: The color to get (if provided).
@@ -337,7 +335,7 @@ def get_product_active_color(product: Product, color: Union[str, None]) -> Attri
 
 def get_product_active_size(active_color: AttributeColor, size: Union[str, None]) -> AttributeSize:
     """
-        Gets the active size for a given product.
+    Gets the active size for a given product.
 
     :param active_color: The active product color to get the active size for.
     :param size: The size to get (if provided).
@@ -354,9 +352,8 @@ def get_product_active_size(active_color: AttributeColor, size: Union[str, None]
 
 def add_or_update_review(form: ReviewsForm, request: WSGIRequest) -> None:
     """
-        Adds a product review if the form is valid and the user is authenticated.
-
-        If the authenticated user has already left a review for the given product,
+    Adds a product review if the form is valid and the user is authenticated.
+    If the authenticated user has already left a review for the given product,
     the existing review is updated with the new values provided in the form.
     Otherwise, a new review is created.
 
--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -1,4 +1,6 @@
 import logging
+from typing import List
+from typing import Union
 
 from django import template
 from django.core.paginator import Paginator
@@ -19,7 +21,11 @@ logger = logging.getLogger(__name__)
 @register.inclusion_tag('shop/inc/list_categories.html')
 def show_category(parent: int = None):
     """
-    Shows subcategories
+    Displays a list of subcategories for the specified parent category.
+
+    :param parent: The ID of the parent category to retrieve subcategories for. If not specified,
+            the top-level categories will be retrieved.
+    :return: A dictionary containing the QuerySet of subcategories to be rendered in the template.
     """
     return {'category': Category.get_parent_categories(parent=parent)}
 
@@ -27,7 +33,10 @@ def show_category(parent: int = None):
 @register.inclusion_tag('shop/inc/banner.html')
 def show_banner(pk_banner: int):
     """
-    Shows banner from Tag
+    Displays a banner on the website using the specified tag.
+
+    :param pk_banner: The ID of the tag that should be used to retrieve the banner.
+    :return: A dictionary containing the tag to be rendered in the template.
     """
     return {'tag': get_tag_by_banner(pk_banner)}
 
@@ -35,7 +44,10 @@ def show_banner(pk_banner: int):
 @register.inclusion_tag('shop/inc/carousel_banner.html')
 def show_carousel_banner(pk_banner: int):
     """
-    Shows carousel banner from Tag
+    Displays a carousel banner on the website using the specified tag.
+
+    :param pk_banner: The ID of the tag that should be used to retrieve the banner.
+    :return: A dictionary containing the tag to be rendered in the template.
     """
     return {'tag': get_tag_by_banner(pk_banner)}
 
@@ -43,7 +55,9 @@ def show_carousel_banner(pk_banner: int):
 @register.inclusion_tag('shop/inc/carousel_brand.html')
 def show_carousel_brand():
     """
-    Shows a carousel of brand logos with available products
+    Displays a carousel of brand logos with available products on the website.
+
+    :return: A dictionary containing the list of active brands to be rendered in the template.
     """
     return {'brand': Manufacturer.get_active_brand()}
 
@@ -51,7 +65,12 @@ def show_carousel_brand():
 @register.inclusion_tag('shop/inc/card_product.html', takes_context=True)
 def show_card_product(context, item):
     """
-    Shows a mini product card
+    Shows a mini product card.
+
+    :param context: The context in which the tag is used, containing the necessary information to
+        render the card.
+    :param item: The product object to be displayed in the card.
+    :return: dict: The context data to be used to render the product card.
     """
     return {'item': item,
             'PRODUCTS_FAVORITE_LIST': context['PRODUCTS_FAVORITE_LIST'],
@@ -63,23 +82,38 @@ def show_card_product(context, item):
 @register.simple_tag()
 def get_products(limit: int = 8, **kwargs) -> QuerySet:
     """
-    Returns products with applied filters
+    Returns a QuerySet of products with applied filters.
+
+    :param limit: The maximum number of products to return.
+    :param kwargs: Filters to apply to the products.
+    :return: A QuerySet of products with applied filters.
     """
     return get_filter_products(limit=limit, **kwargs)
 
 
 @register.simple_tag()
-def get_all_categories():
+def get_all_categories() -> QuerySet:
     """
-    Gets all categories
+    Returns a QuerySet of all categories.
+
+    :return: A QuerySet of all categories.
     """
     return Category.get_all_categories()
 
 
 @register.simple_tag
-def get_proper_elided_page_range(paginator, number, on_each_side=1, on_ends=1):
+def get_proper_elided_page_range(paginator, number, on_each_side=1, on_ends=1) -> List[int]:
     """
-    Returns the paginator
+    Returns a list of page numbers for the paginator, with ellipses to indicate
+    hidden pages.
+
+    :param paginator: The paginator object.
+    :param number: The current page number.
+    :param on_each_side: The number of page numbers to display on each side of the
+        current page number.
+    :param on_ends: The number of page numbers to display at the beginning and end
+        of the page range.
+    :return: A list of page numbers for the paginator.
     """
     paginator = Paginator(paginator.object_list, paginator.per_page)
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side,
@@ -87,9 +121,15 @@ def get_proper_elided_page_range(paginator, number, on_each_side=1, on_ends=1):
 
 
 @register.simple_tag()
-def get_user_review(user_id: int, product_id: int):
+def get_user_review(user_id: int, product_id: int) -> Union[None, int]:
     """
-    Returns the rating given by the user of the product
+    Returns the rating given by the user for the product, or None if the user
+    has not provided a rating for the product.
+
+    :param user_id: The ID of the user.
+    :param product_id: The ID of the user.
+    :return: The rating given by the user for the product, or None if the user
+             has not provided a rating for the product
     """
     return get_review_for_user_and_product(user_id=user_id, product_id=product_id)
 
@@ -97,6 +137,9 @@ def get_user_review(user_id: int, product_id: int):
 @register.simple_tag()
 def get_fa_star(rating: int = 5) -> str:
     """
-    Draws product rating stars based on average rating
+    Returns the HTML for a star icon to display the rating.
+
+    :param rating: The rating to display, from 0 to 5.
+    :return: The HTML for a star icon to display the rating.
     """
     return get_rating_html(rating)
