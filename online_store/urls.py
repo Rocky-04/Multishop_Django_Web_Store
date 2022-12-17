2405e456fa1a2218ad9dc59d384f3150b767b2ef
Rocky-04 <olegtsynkovskiy@gmail.com>
2022-12-17 16:17:48 +0200

    debug=false

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,15 +1,3 @@
-"""online_store URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-"""
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
